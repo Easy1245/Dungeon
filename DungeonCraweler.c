f9cd7a19846a535ebdafe9058042d3815c2b851a
Easy1245 <jonasvanmarsenille@gmail.com>
2025-05-25 21:22:16 +0200

    typedef toegevoegd

--- DungeonCraweler.c
+++ DungeonCraweler.c
@@ -9,3 +9,29 @@
 #define MAX_CONNECTIONS 4
 #define INPUT_SIZE 32
 #define MAX_ITEMS 5
+
+typedef struct Monster {
+    char name[MAX_NAME];
+    int health;
+} Monster;
+
+typedef struct Room {
+    int id;
+    char name[MAX_NAME];
+    char description[MAX_DESC];
+    struct Room* connections[MAX_CONNECTIONS];
+    char* items[MAX_ITEMS];
+    Monster* monster;
+    bool hasTreasure;
+} Room;
+
+typedef struct Player {
+    char name[MAX_NAME];
+    Room* currentRoom;
+    char** inventory;      
+    int inventoryCount;    
+    int inventoryCapacity; 
+    int health;
+    int damage;
+    int defense;
+} Player;
