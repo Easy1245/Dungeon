ab403f84d53e2187c33a387482f1d12232785d3e
Easy1245 <jonasvanmarsenille@gmail.com>
2025-05-25 21:18:14 +0200

    DungeonCraweler.c toegevoed

---
+++ DungeonCraweler.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define MAX_NAME 32
+#define MAX_DESC 128
+#define MAX_CONNECTIONS 4
+#define INPUT_SIZE 32
+#define MAX_ITEMS 5
